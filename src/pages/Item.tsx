a5ee1222e4750090b4420c4c246fefb74e2c0fdd
RuvindaD <rdilleperuma@gmail.com>
2024-10-25 20:35:15 +0530

    Second int

--- src/pages/Item.tsx
+++ src/pages/Item.tsx
@@ -41,9 +41,10 @@ function Item(){
 
 
     useEffect(function(){
-        if(isAuthenticated)
+        if(isAuthenticated){
         loadItems();
         loadCategories();
+        }
     },[isAuthenticated])
 
     
